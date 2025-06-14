5f31570ac3b7e4d046a07da27544d1465af67081
Maximlian <chicagomaz13@gmail.com>
2025-06-14 17:04:49 -0400

    initial app functionality Navbar and home page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import MobileMenu from "./MobileMenu";
+import Image from "next/image";
+
+
+export default function Navbar() {
+    return (
+        <div className="h-24 flex items-center justify-between">
+            {/* LEFT */}
+            <div className="md:hidden lg:block w-[20%]">
+                <Link href='\' className="font-bold text-xl text-blue-500">MONKEYSOCIAL</Link>
+            </div>
+            {/* CENTER */}
+            <div className="hidden md:flex w-[50%] text-sm">
+                {/* LINKS */}
+                <div className="flex gap-6 text-gray-600">
+                    <Link href="/" className="flex items-center gap-2">
+                    <Image src="/home.png" alt="Homepage" width={16} height={16} className="w-4 h-4"/>
+                    <span>Homepage</span>
+                    </Link>
+                    <Link href="/" className="flex items-center gap-2">
+                    <Image src="/friends.png" alt="Friends" width={16} height={16} className="w-4 h-4"/>
+                    <span>Friends</span>
+                    </Link>
+                    <Link href="/" className="flex items-center gap-2">
+                    <Image src="/stories.png" alt="Stories" width={16} height={16} className="w-4 h-4"/>
+                    <span>Stories</span>
+                    </Link>
+                    
+                </div>
+            </div>
+            {/* RIGHT */}
+            <MobileMenu/>
+        </div>
+    );
+}
